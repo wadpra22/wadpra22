b3ba791dda205406390230181f1341c38c8ab3f6
wadpra22 <140890071+wadpra22@users.noreply.github.com>
2024-03-29 10:39:55 +0530

    Create tribute.h

---
+++ tribute.h
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+    <style>
+    </style>
+  </head>
+  <body>
+    <h2 style="font-size:30px;background-color: aqua;color:rgb(5, 5, 5);font-family:aerial;text-align:center;">Tribute To Sarojini Naidu</h2>
+    <iframe width="560" height="315" src="https://images.cnbctv18.com/wp-content/uploads/2022/08/Sarojini-Naidu-1019x573.jpg?impolicy=website&width=590&height=264"></iframe>
+    <hr>
+    <p><b>Sarojini Naidu</b> (13 February 1879 – 2 March 1949)[1] was an Indian political activist and poet who served as the first Governor of United Provinces, after India's independence. She played an important role in the Indian independence movement against the British Raj. She was the first indian woman to be president of the Indian National Congress and appointed as governor of a state.</p>
+    <h2>Early Life</h2>
+    <p><b>Sarojini Naidu Information
+        Sarojini Naidu Birthday - February 13th, 1879
+        Sarojini Naidu Birthplace - Hyderabad, India
+        Sarojini Naidu Husband Name - Govindarajulu Naidu
+        Sarojini Naidu Death Date - March 2nd, 1949
+        Sarojini Naidu Death Cause - Cardiac arrest</b></p>
+    <p>Education Naidu was born in Hyderabad on the 13th of February 1879 to eminent linguist Aghornath Chattopadhyay and his wife Barada Sundari Devi, a Bengali poetess. Her father was also one of the first members of the Indian National Congress in Hyderabad. 
+        Sarojini Naidu was an intelligent student who showed fluency in Urdu, Telugu, English, Bengali, and Persian. 
+        At the age of 12, she received fame by topping the matriculation exams of Madras University. This leads her to receive a scholarship from the Nizam of Hyderabad to study abroad.
+        Naidu was interested in writing poetry, while her father wanted her to be a mathematician.
+        Sarojini went to study in England where she met famous literary laureates such as Edmond Goose and Arthur Symons. Goose suggested that Naidu should use Indian themes in her poetry work. 
+        Naidu expressed the life and events of modern India through her poetry. Her works- 'The Golden Threshold’ (1905), ‘The Bird of Time’ (1912), and ‘The Broken Wing’ (1917) found readership in both India and England.
+        Sarojini Naidu had an intercaste marriage with Dr. Muthyala Govindarajulu Naidu, a South Indian doctor under the  Brahmo Marriage Act (1872) </p>
+        <center><a href="https://www.jagranjosh.com/general-knowledge/sarojini-naidu-biography-1646201899-1">Learn More About Sarojini Naidu</a></center>                                                                                                                                                                                 
